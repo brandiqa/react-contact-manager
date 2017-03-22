368e674d54ed98f79e1c51201dea0034a2415931
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-22 18:30:06 +0300

    Added skeleton crud pages

---
+++ src/pages/contacts-form-page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactsFormPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Contacts</h1>
+      </div>
+    )
+  }
+}
+
+export default ContactsFormPage;
---
+++ src/pages/contacts-list-page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactsListPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contacts List</h1>
+      </div>
+    )
+  }
+}
+
+export default ContactsListPage;
