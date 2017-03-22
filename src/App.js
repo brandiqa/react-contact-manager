1ba4348d589b8f1c5b37d71026d45237d184c8e5
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-22 18:42:42 +0300

    Implemented routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import ContactsListPage from './pages/contacts-list-page';
+import ContactsFormPage from './pages/contacts-form-page';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>React Contact Manager</h1>
-      </div>
+      <div className="ui container">
+          <div className="ui two item menu">
+            <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
+            <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
+          </div>
+          <Route exact path="/" component={ContactsListPage}/>
+          <Route path="/contacts/new" component={ContactsFormPage}/>
+          <Route path="/contacts/edit/:_id" component={ContactsFormPage}/>
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+      <App />
+  </BrowserRouter> ,
   document.getElementById('root')
 );
