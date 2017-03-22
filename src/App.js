4bb6ae6660a18a1b3b57573d3f2dadcb2d171d93
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-22 18:48:20 +0300

    Replaced css container with react container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import ContactsListPage from './pages/contacts-list-page';
 import ContactsFormPage from './pages/contacts-form-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="ui container">
-          <div className="ui two item menu">
-            <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
-            <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
-          </div>
-          <Route exact path="/" component={ContactsListPage}/>
-          <Route path="/contacts/new" component={ContactsFormPage}/>
-          <Route path="/contacts/edit/:_id" component={ContactsFormPage}/>
+      <Container>
+        <div className="ui two item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
         </div>
+        <Route exact path="/" component={ContactsListPage}/>
+        <Route path="/contacts/new" component={ContactsFormPage}/>
+        <Route path="/contacts/edit/:_id" component={ContactsFormPage}/>
+      </Container>
     );
   }
 }
