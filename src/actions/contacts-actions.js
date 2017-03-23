aa6196cfbff4ac60431c14c792e766ee2da88a51
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-23 17:26:37 +0300

    Refactored axios-client code

--- src/actions/contacts-actions.js
+++ src/actions/contacts-actions.js
@@ -1,24 +1,14 @@
-import axios from "axios";
+import { client } from './';
 
 export const FETCH_CONTACTS_PENDING = 'FETCH_CONTACTS_PENDING';
 export const FETCH_CONTACTS_FULFILLED = 'FETCH_CONTACTS_FULFILLED';
 export const FETCH_CONTACTS_REJECTED = 'FETCH_CONTACTS_REJECTED';
-
-const client = axios.create({
-  baseURL: "http://localhost:3030",
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
 
 export function fetchContacts() {
   return dispatch => {
     dispatch({
       type: 'FETCH_CONTACTS',
       payload: client.get("/api/contacts")
-    })
-    .catch(err => {
-      console.log(err)
     })
   }
 }
