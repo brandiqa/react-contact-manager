8543a61993fcfdea76bc86b8321b6a2068bfc11f
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-23 14:45:40 +0300

    setup contacts fetching in reducer

--- src/reducers/contacts-reducer.js
+++ src/reducers/contacts-reducer.js
@@ -1,3 +1,5 @@
+import { FETCH_CONTACTS, FETCH_CONTACTS_FULFILLED, FETCH_CONTACTS_REJECTED } from '../actions/contacts-actions';
+
 const defaultState = {
   contacts: [],
   fetching: false,
@@ -7,6 +9,24 @@ const defaultState = {
 
 export default (state=defaultState, action={}) => {
   switch (action.type) {
+    case FETCH_CONTACTS:
+      return {...state, fetching: true}
+
+    case FETCH_CONTACTS_FULFILLED:
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        contacts: action.payload,
+      }
+
+    case FETCH_CONTACTS_REJECTED:
+      return {
+        ...state,
+        fetching: false,
+         error: action.payload
+       }
+
     default:
       return state;
   }
