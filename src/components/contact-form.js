50c4cc3152ec1a82ac1e95e132b3f747e70570a6
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-25 12:57:24 +0300

    Refactored ContactForm into redux-form requirements

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,81 +1,120 @@
 import React, { Component } from 'react';
 import { Form, Grid, Button } from 'semantic-ui-react';
 import classnames from 'classnames';
+import { Field, reduxForm } from 'redux-form';
 
 class ContactForm extends Component {
 
-  state = {
-    _id: this.props.contact ? this.props.contact._id : null,
-    first: this.props.contact ? this.props.contact.name.first : '',
-    last: this.props.contact ? this.props.contact.name.last : '',
-    phone: this.props.contact ? this.props.contact.phone : '',
-    email: this.props.contact ? this.props.contact.email : '',
-    pageTitle: this.props.contact ? 'Edit Contact' : 'Add Contact',
-    errors: {},
-    loading: false
-  }
+  // state = {
+  //   _id: this.props.contact ? this.props.contact._id : null,
+  //   first: this.props.contact ? this.props.contact.name.first : '',
+  //   last: this.props.contact ? this.props.contact.name.last : '',
+  //   phone: this.props.contact ? this.props.contact.phone : '',
+  //   email: this.props.contact ? this.props.contact.email : '',
+  //   pageTitle: this.props.contact ? 'Edit Contact' : 'Add Contact',
+  //   errors: {},
+  //   loading: false
+  // }
+  //
+  // handleChange = (e) => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
+  //
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Todo: Validate Form
+  //   const { _id, first, last, phone, email } = this.state;
+  //   const contact = {_id, name: { first, last}, phone, email}
+  //   // Save contact
+  //   this.setState({loading: true})
+  //   this.props.saveContact(contact);
+  // }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // Todo: Validate Form
-    const { _id, first, last, phone, email } = this.state;
-    const contact = {_id, name: { first, last}, phone, email}
-    // Save contact
-    this.setState({loading: true})
-    this.props.saveContact(contact);
-  }
+  // render() {
+  //   return (
+  //     <Grid centered columns={2} >
+  //       <Grid.Column>
+  //         <h1 style={{marginTop:"1em"}}>{this.state.pageTitle}</h1>
+  //         <Form className={classnames('ui', 'form', {loading: this.state.loading })} onSubmit={this.handleSubmit}>
+  //
+  //           {!!this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div>}
+  //
+  //           <div className={classnames('field', {error: !!this.state.errors.first})}>
+  //             <Form.Input
+  //               label='First name'
+  //               placeholder='First name'
+  //               name="first"
+  //               value={this.state.first}
+  //               onChange={this.handleChange}
+  //             />
+  //             <span>{this.state.errors.first}</span>
+  //           </div>
+  //           <div className={classnames('field', {error: !!this.state.errors.last})}>
+  //             <Form.Input
+  //               label='Last name'
+  //               placeholder='Last name'
+  //               name="last"
+  //               value={this.state.last}
+  //               onChange={this.handleChange}
+  //              />
+  //           </div>
+  //           <div className={classnames('field', {error: !!this.state.errors.phone})}>
+  //             <Form.Input
+  //               label='Phone'
+  //               placeholder="phone"
+  //               name="phone"
+  //               value={this.state.phone}
+  //               onChange={this.handleChange}
+  //             />
+  //           </div>
+  //           <div className={classnames('field', {error: !!this.state.errors.email})}>
+  //             <Form.Input
+  //               label='Email'
+  //               placeholder="email"
+  //               name="email"
+  //               value={this.state.email}
+  //               onChange={this.handleChange}
+  //             />
+  //           </div>
+  //           <Button type='submit' primary>Save</Button>
+  //         </Form>
+  //       </Grid.Column>
+  //     </Grid>
+  //   )
+  // }
 
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <Grid centered columns={2} >
+      <Grid centered columns={2}>
         <Grid.Column>
-          <h1 style={{marginTop:"1em"}}>{this.state.pageTitle}</h1>
-          <Form className={classnames('ui', 'form', {loading: this.state.loading })} onSubmit={this.handleSubmit}>
-
-            {!!this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div>}
-
-            <div className={classnames('field', {error: !!this.state.errors.first})}>
-              <Form.Input
-                label='First name'
-                placeholder='First name'
-                name="first"
-                value={this.state.first}
-                onChange={this.handleChange}
-              />
-              <span>{this.state.errors.first}</span>
+          <h1 style={{marginTop:"1em"}}>Add New Contact</h1>
+          <Form onSubmit={handleSubmit}>
+            <div>
+              <Form.Field>
+                <label htmlFor="first">First Name</label>
+                <Field name="first" component="input" type="text"/>
+              </Form.Field>
             </div>
-            <div className={classnames('field', {error: !!this.state.errors.last})}>
-              <Form.Input
-                label='Last name'
-                placeholder='Last name'
-                name="last"
-                value={this.state.last}
-                onChange={this.handleChange}
-               />
+            <div>
+              <Form.Field>
+                <label htmlFor="last">Last Name</label>
+                <Field name="last" component="input" type="text"/>
+              </Form.Field>
             </div>
-            <div className={classnames('field', {error: !!this.state.errors.phone})}>
-              <Form.Input
-                label='Phone'
-                placeholder="phone"
-                name="phone"
-                value={this.state.phone}
-                onChange={this.handleChange}
-              />
+            <div>
+              <Form.Field>
+                <label htmlFor="phone">Phone</label>
+                <Field name="phone" component="input" type="text"/>
+              </Form.Field>
             </div>
-            <div className={classnames('field', {error: !!this.state.errors.email})}>
-              <Form.Input
-                label='Email'
-                placeholder="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
+            <div className={classnames('field', {error: !!this.props.errors})}>
+              <Form.Field>
+                <label htmlFor="email">Last Name</label>
+                <Field name="email" component="input" type="email"/>
+              </Form.Field>
             </div>
             <Button type='submit' primary>Save</Button>
           </Form>
@@ -85,8 +124,10 @@ class ContactForm extends Component {
   }
 }
 
-ContactForm.propTypes = {
-  saveContact: React.PropTypes.func.isRequired
-}
+// ContactForm.propTypes = {
+//   saveContact: React.PropTypes.func.isRequired
+// }
 
-export default ContactForm;
+// ContactForm = reduxForm({form: 'contact'})(ContactForm);
+
+export default reduxForm({form: 'contact'})(ContactForm);
